d4ef8339c59c3585785c97b94e5def7667009e5e
Mampionona33 <ramamps33@gmail.com>
2023-09-10 18:52:59 +0300

    add models to the admin interface

--- app/sekoly/admin.py
+++ app/sekoly/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Etudiant, Classe, ClasseMatiere, Filiere, Niveau, Matiere, Coeff, CoeffMatiereFiliere
+
+# Enregistrez vos modèles ici.
+admin.site.register(Etudiant)
+admin.site.register(Classe)
+admin.site.register(ClasseMatiere)
+admin.site.register(Filiere)
+admin.site.register(Niveau)
+admin.site.register(Matiere)
+admin.site.register(Coeff)
+admin.site.register(CoeffMatiereFiliere)
