981e762150624d9244dda1b584bfdbeae490ba9a
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-11 06:52:19 +0000

    Update 3 files: start setup login

--- app/sekoly/views.py
+++ app/sekoly/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
+# def index(request):
+#     return render(request, "sekoly/index.html")
+
 def index(request):
-    return render(request, "sekoly/index.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # Connectez l'utilisateur
+            return render(request, "sekoly/index.html")
+    
+    # Si l'utilisateur n'est pas connecté ou la connexion a échoué, redirigez-le vers la page de connexion
+    return redirect("login")
 
 
