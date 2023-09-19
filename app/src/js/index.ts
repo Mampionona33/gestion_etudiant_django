b1812ddb4bf87edb3c304706a9e54debee6cc32c
Mampionona33 <ramamps33@gmail.com>
2023-09-19 21:58:28 +0300

    try to set up

--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -1,6 +1,14 @@
 import "../styles/style.scss";
+import AlertHandler, { AlertType } from "./AlertHandler.ts";
 import ToastHandler, { ToastType } from "./ToastHandler.ts";
 import * as bootstrap from "bootstrap";
+
+const alertDelete = new AlertHandler(
+  "btn-delete11",
+  "delete",
+  "Êtes-vous sûr de vouloir supprimer cette classe",
+  AlertType.Danger
+);
 
 document.addEventListener("DOMContentLoaded", function () {
   // Récupérez la valeur de error_message de manière appropriée, par exemple en l'ajoutant comme attribut data dans un élément HTML
@@ -29,4 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
       toastHandleMessageInfo.render();
     }
   }
+
+  alertDelete.render();
 });
